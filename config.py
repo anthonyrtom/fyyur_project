578805ce0a190c49d8b81db61d272b70f5b71070
Antony <anthonyrtom@yahoo.com>
2022-03-26 16:58:20 +0200

    first revision of app after code review, changes in the config file

--- config.py
+++ config.py
@@ -7,8 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+DB_HOST = os.environ.get('DB_HOST','127.0.0.1:5432')
+DB_NAME = os.environ.get('DB_NAME','fyyurdb')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'pass123')
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass123@localhost:5432/fyyurdb'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass123@localhost:5432/fyyurdb'
+SQLALCHEMY_DATABASE_URI = DB_PATH
 SQLALCHEMY_TRACK_MODIFICATIONS = False
